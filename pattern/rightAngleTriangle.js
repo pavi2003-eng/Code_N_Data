681bf2154e47afe01173685fd60f3d9375a4037f
PAVITHRA VIJAYARANGAN <duraiv2008@gmail.com>
2024-12-22 14:36:52 +0530

    Delete pattern directory

--- pattern/rightAngleTriangle.js
+++
@@ -1,8 +0,0 @@
-let row=prompt("Enter Number of rows:")
-for(let i=0;i<=row;i++){
-    let pattern=""
-    for(let j=0;j<=i;j++){
-        pattern+="* "
-    }
-    console.log(pattern)
-}
--- pattern/square.js
+++
@@ -1,8 +0,0 @@
-let row=prompt("Enter Number of rows:")
-for(let i=0;i<=row;i++){
-    let pattern=""
-    for(let j=0;j<=row;j++){
-        pattern+="* "
-    }
-    console.log(pattern)
-}
