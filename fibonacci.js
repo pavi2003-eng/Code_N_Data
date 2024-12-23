3ac5920b34dd75bd3e76d2a1efd20d7b037a5e21
PAVITHRA <duraiv2008@gmail.com>
2024-12-23 13:34:04 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
+console.log("----Fibonacci Series within n----")
 n=100
 let arr=[]
-count=0
 for(let i=0;i<=n;i++){
     if(i==0||i==1)
     {
@@ -14,3 +14,19 @@ for(let i=0;i<=n;i++){
     }
 }
 console.log(arr)
+
+console.log("----Fibonacci Series upto n----")
+let arr1=[]
+for(let i=0;i<=n;i++){
+    if(i==0||i==1)
+    {
+        arr1.push(i)
+    }
+    else{
+        let series=arr1[i-1]+arr1[i-2]
+        if(arr1.length<n){
+            arr1.push(series)
+        }
+    }
+}
+console.log(arr1)
