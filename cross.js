d7882e7142f1c76c2bc5d9530a242b0a1f7538e6
PAVITHRA <duraiv2008@gmail.com>
2024-12-27 19:55:07 +0530

    Update cross.js

--- cross.js
+++ cross.js
@@ -2,7 +2,7 @@ let row=prompt("Enter Number of rows:")
 for(let i=0;i<=row;i++){
     let pattern=""
     for(let j=0;j<=row;j++){
-       (i==j)||(i+j==row) ? pattern+="*" : pattern+=" "
+       (i==j)||(i+j==row) ? pattern+="* " : pattern+=" "
     }
     console.log(pattern)
 }
